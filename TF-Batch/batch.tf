c14e45d49cdc21e4133c6bd660e384a27a59d8b4
amuhtarov <amuhtarov@akumoproject.com>
2024-02-02 19:41:59 -0600

    test

--- TF-Batch/batch.tf
+++ TF-Batch/batch.tf
@@ -1,10 +1,83 @@
+//----------------------IAM Roles----------------------//
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "ecs_instance_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_role" {
+  name = "ecs_instance_role"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+data "aws_iam_policy_document" "batch_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["batch.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "aws_batch_service_role" {
+  name               = "aws_batch_service_role"
+  assume_role_policy = data.aws_iam_policy_document.batch_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
+  role       = aws_iam_role.aws_batch_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "tf_test_batch_exec_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 //----------------------COMPUTE INVIRONMENT----------------------//
 resource "aws_batch_compute_environment" "demo" {
   compute_environment_name = "demo"
 
   compute_resources {
-    instance_role = "arn:aws:iam::${data.aws_caller_identity.current.id}:instance-profile/AWS-Batch-EC2-Role"
+    instance_role = aws_iam_instance_profile.ecs_instance_role.arn
 
     instance_type = [
       "optimal"
@@ -26,7 +99,7 @@ resource "aws_batch_compute_environment" "demo" {
     type = "EC2"
   }
 
-  service_role = "arn:aws:iam::${data.aws_caller_identity.current.id}:role/aws-service-role/batch.amazonaws.com/AWSServiceRoleForBatch"
+  service_role = aws_iam_role.aws_batch_service_role.arn
   type         = "MANAGED"
 }
 
@@ -35,6 +108,7 @@ resource "aws_batch_job_definition" "test" {
   name = "demo-batch-definitions"
   type = "container"
   container_properties = jsonencode({
+    jobRoleArn = "arn:aws:iam::${data.aws_caller_identity.current}:role/AWS-Terraform-Role"
     image   = "public.ecr.aws/s6a4j9d6/demo-dock:latest",
     resourceRequirements = [
       {
@@ -46,6 +120,8 @@ resource "aws_batch_job_definition" "test" {
         value = "500"
       }
     ]
+
+    executionRoleArn = aws_iam_role.ecs_task_execution_role.arn
   })
 }
 
