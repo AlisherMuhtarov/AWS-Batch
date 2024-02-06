f12f582bbaa93c21b87d49461674586627b41d6c
amuhtarov <amuhtarov@akumoproject.com>
2024-02-06 14:54:48 -0600

    added content into starter-data

--- Starter-Data.sh
+++ Starter-Data.sh
@@ -1,3 +1,5 @@
+# Used to randomly generate customer data
+
 for i in {1..30}; do
     echo "Customer ID: $i" > Customer-Data/file$i.txt
     echo "Customer Name: Customer$i" >> Customer-Data/file$i.txt
